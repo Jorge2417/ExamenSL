871526a1e68cbd7415fd9a606bd7fe45b643b93a
xmiloxp <m.lopez@unas.edu.pe>
2019-05-10 10:02:21 -0500

    fix #2

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const arbolRoutes = require('./Routes/arbol');
 
 // settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Presentacion'));
 app.set('view engine', 'ejs');
 
 app.use(myConnection(mysql, {
